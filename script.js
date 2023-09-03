2a2a572edccb3efa72c8eb7e7df0dffff125caf3
aakinsin <aakinsin02@gmail.com>
2023-09-03 23:28:49 +0300

    изменил js

--- script.js
+++ script.js
@@ -1,74 +1,43 @@
 //Задание 1
-let password = 'password';
+let i = 0;
+while (i < 2) {
+    console.log('Привет!');
+    i++;
 
-let pass = prompt('Введите пароль');
-if (password === pass) {
-    console.log('Пароль введен верно');
-} else {
-    console.log('Пароль введен неправильно');
 }
 //Задание 2
-let c = 5;
-if (c > 0 && c < 10) {
-    console.log('Верно');
-} else {
-    console.log('Неверно');
+while (i < 7) {
+    console.log(i - 1);
+    i++;
 }
 //Задание 3
-let d = 50;
-let e = 110;
-if (d > 100 || e > 100) {
-    console.log('Верно');
-} else {
-    console.log('Неверно');
+while (i <= 22) {
+    console.log(i);
+    i++;
 }
 //Задание 4
-let a = '2';
-let b = '3';
-// Код выше изменять менять нельзя, чтобы решить задачу исправьте код ниже: 
-a = Number(a);
-b = Number(b);
-alert(a + b);
-//Задание 5
-let monthNumber = Number(prompt('Напишите номер месяца'));
-switch (monthNumber) {
-    case 1:
-        console.log('Зима');
-        break;
-    case 2:
-        console.log('Зима');
-        break;
-    case 3:
-        console.log('Весна');
-        break;
-    case 4:
-        console.log('Весна');
-        break;
-    case 5:
-        console.log('Весна');
-        break;
-    case 6:
-        console.log('Лето');
-        break;
-    case 7:
-        console.log('Лето');
-        break;
-    case 8:
-        console.log('Лето');
-        break;
-    case 9:
-        console.log('Осень');
-        break;
-    case 10:
-        console.log('Осень');
-        break;
-    case 11:
-        console.log('Осень');
-        break;
-    case 12:
-        console.log('Зима');
-        break;
-    default:
-        console.log('Такого месяца нет');
-        break;
+const obj = {
+    'Коля': '200',
+    'Вася': '300',
+    'Петя': '400'
 }
+for (const man in obj) {
+    console.log(man, '— зарплата ', obj[man], ' долларов');
+}
+//Задание 5
+let n = 1000;
+let num = 0;
+while (n >= 50) {
+    n /= 2;
+    num++;
+}
+console.log(n, num);
+//Задание 6
+let day = prompt("Число первой пятницы месяца?");
+let fri = day % 7;
+while (day <= 31) {
+    if (day % 7 == fri) {
+        console.log('Сегодня пятница,', day, '-е число. Необходимо подготовить отчет.');
+    };
+    day++;
+};
